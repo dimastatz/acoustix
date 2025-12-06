efed5a53c98687b4c0bb8f8152d1f1a4004290da
dimastatz <dima.statz@gmail.com>
2025-12-06 05:42:34 +0200

    add run.sh

---
+++ run.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+abort()
+{
+    echo "*** FAILED ***" >&2
+    exit 1
+}
+
+if [ "$#" -eq 0 ]; then
+    echo "Wrong argument is provided. Usage:
+            '-local' to build local environment
+            '-test' to run linter, formatter and tests
+            '-docker' to build and run docker image"
+
+elif [ $1 = "-local" ]; then
+    trap 'abort' 0
+    set -e
+    echo "Running format, linter and tests"
+    rm -rf .venv
+    python3 -m venv .venv
+    source .venv/bin/activate
+    pip install --upgrade pip
+    pip install -r ./requirements.txt
+
+    black acoustix tests
+    pylint --fail-under=9.9 acoustix tests
+    pytest --ignore=tests/benchmark --cov-fail-under=95 --cov acoustix -v tests
+elif [ $1 = "-test" ]; then
+    trap 'abort' 0
+    set -e
+    
+    echo "Running format, linter and tests"
+    source .venv/bin/activate
+    black acoustix tests
+    pylint --fail-under=9.9 acoustix tests
+    pytest --ignore=tests/benchmark --cov-fail-under=95 --cov --log-cli-level=INFO acoustix -v tests
+elif [ $1 = "-docker" ]; then
+    echo "Building and running docker image"
+    docker stop acoustix-container
+    docker rm acoustix-container
+    docker rmi acoustix-image
+    # build docker
+    docker build --tag acoustix-image --build-arg CACHEBUST=$(date +%s) . --file Dockerfile.test
+elif [ $1 = "-deploy-package" ]; then
+    echo "Running WhisperFlow package setup"
+    pip install twine
+    pip install wheel
+    python setup.py sdist bdist_wheel
+    rm -rf .venv_test
+    python3 -m venv .venv_test
+    source .venv_test/bin/activate
+    pip install ./dist/acoustix-0.2-py3-none-any.whl
+    pytest --ignore=tests/benchmark --cov-fail-under=95 --cov acoustix -v tests
+    # twine upload ./dist/*
+else
+  echo "Wrong argument is provided. Usage:
+            '-local' to build local environment
+            '-test' to run linter, formatter and tests
+            '-docker' to build and run docker image"
+fi
+
+trap : 0
+echo >&2 '*** DONE ***'
