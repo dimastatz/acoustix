56234dd6079a5efc37151cdd400444f6514792f1
dimastatz <dima.statz@gmail.com>
2025-12-07 06:13:55 +0200

    get the file

--- tests/test_audio.py
+++ tests/test_audio.py
@@ -1,4 +1,6 @@
 """ test audio module """
+from tests.utilities import get_resource_path
+from acoustix.core.audio import get_audio_info
 from acoustix.core.audio import compare_voice_similarity
 
 
@@ -6,3 +8,21 @@ def test_compare_voice_similarity():
     """test compare_voice_similarity function"""
     score = compare_voice_similarity("voice_sample_1.wav", "voice_sample_2.wav")
     assert 0.0 <= score <= 1.0
+
+
+def test_get_audio_info():
+    """test get_audio_info function"""
+
+    # prepare test file path
+    test_file = get_resource_path("3081-166546-0000", "wav")
+
+    # call the function
+    info = get_audio_info(test_file)
+
+    # assertions
+    assert isinstance(info, dict)
+    assert "duration_sec" in info
+    assert "sample_rate" in info
+    assert "channels" in info
+    assert "frames" in info
+    assert "codec" in info
---
+++ tests/utilities.py
@@ -0,0 +1,9 @@
+""" test utilities """
+import os
+
+
+def get_resource_path(name: str, extension: str) -> str:
+    "get resources path"
+    current_path = os.path.dirname(__file__)
+    path = os.path.join(current_path, f"./resources/{name}")
+    return f"{path}.{extension}"
